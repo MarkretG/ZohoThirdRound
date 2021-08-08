782ffb65a0b3dadb079220850fb21504589b6bf2
MarkretG <markretg@gmail.com>
2021-08-08 20:38:11 +0530

    initial commit

---
+++ src/bankingSystem/BankingManagementSystem.java
@@ -0,0 +1,19 @@
+package bankingSystem;
+import java.util.Scanner;
+public class BankingManagementSystem {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("welcome to banking management system");
+        System.out.println("1.New Customer\n2.Add new account for existing customer\n3.get account info for given customer_id\n4.exit");
+        while (true)
+        {
+            int choice=scanner.nextInt();
+            switch (choice)
+            {
+                case 1:
+
+            }
+
+        }
+    }
+}
